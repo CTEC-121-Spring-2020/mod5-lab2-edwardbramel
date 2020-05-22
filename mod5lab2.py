f00341ed47c70c00a38660528c2a32e844aa8084
Eddy Bramel <e.lamanna-bramel@students.clark.edu>
2020-05-21 19:08:59 -0700

    made some more code just for myself

--- mod5lab2.py
+++ mod5lab2.py
@@ -10,14 +10,14 @@ Input(s): family names
 Process: asks what each fmaily a question of two
 Output: puts the name of each value in a function
 """
-
+# code from class
+"""
 
 def verse(name):
     print(name, "finger,", name, "finger, where are you?")
 
 
 def refrain():
-
     print("here i am, here i am, how do you do?")
     print()
 
@@ -29,6 +29,38 @@ def sinfamilysong():
     verse("brother")
     refrain()
     verse("mother")
+    refrain()
+    verse("sister")
+    refrain()
+    verse("baby")
+    refrain()
+
+
+def main():
+    sinfamilysong()
+
+
+main()
+"""
+# making one for myself to practice
+
+
+def verse(name):
+    print("hello ", name, ", im testing for mode")
+
+
+def refrain():
+    print("wonderfull weather were having today")
+    print()
+
+
+def sinfamilysong():
+    print()
+    verse("motha")
+    refrain()
+    verse("motha agian")
+    refrain()
+    verse("cuuuusin")
     refrain()
     verse("sister")
     refrain()
